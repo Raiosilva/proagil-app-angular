b33f09c76189cfefdd49910697d1009c171e3aa7
Raiosilva <raimundopi025@gmail.com>
2019-06-14 12:36:29 -0300

    improving project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+
+import { EventoService } from './_services/evento.service';
+
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos';
+import { NavComponent } from './nav/nav.component';
+
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      EventosComponent,
+      NavComponent,
+      DateTimeFormatPipePipe
+   ],
+   imports: [
+      BrowserModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot(),
+      AppRoutingModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+    EventoService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
